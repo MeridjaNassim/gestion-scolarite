482bd51e7ab462e970f9cc8f29b7dbfdaea847a8
Mohamed Abdelmadjid Boudis <hm_boudis@esi.dz>
2019-11-24 23:20:52 +0100

    Init : Dashs

---
+++ client/src/components/ThesDash.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../css/Dash.css";
+
+class ThesDash extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <>
+            </>
+        );
+    }
+}
+
+export default ThesDash;
