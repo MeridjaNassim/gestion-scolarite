7fa0b89c29f47d31408cff8a9566aa56bc8a7db3
MeridjaNassim <ha_meridja@esi.dz>
2020-01-03 00:04:18 +0100

    started Backend doc

--- server/gateway/index.js
+++ server/gateway/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const router = require("./routers/router");
+
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./docs/swagger.json");
 app.use(express.json());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -14,5 +17,6 @@ app.use((req, res, next) => {
 });
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get("/", (req, res) => res.send("Gateway for microservices!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
